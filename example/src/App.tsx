b99c78001d94d9a8042efebe78b7429d58cbd3c6
waynekim <eunchan0817@gmail.com>
2023-04-18 15:08:07 +0900

    feat: implement case - element is completely contained in the viewport

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,34 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-on-viewport';
+import { View, ScrollView, ColorValue } from 'react-native';
+import { IsOnViewport, OnViewport } from 'react-native-on-viewport';
+
+const Box = ({ backgroundColor }: { backgroundColor: ColorValue }) => <View
+  style={{ backgroundColor, height: 300, width: 400 }} />;
+
+const colors = ['blue', 'red', 'green', 'yellow', 'black'];
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
+  // @ts-ignore
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View>
+      <OnViewport>
+        <ScrollView style={{ flexGrow: 1, flexShrink: 1 }} scrollEventThrottle={1}>
+          {colors.map((color, index) => (
+            <IsOnViewport
+              id={`${index}${color}`}
+              onViewport={(status) => {
+                if (status) console.log(`${color} IN`);
+                else console.log(`${color} OUT`);
+              }}
+              key={index}
+            >
+              <Box backgroundColor={color} />
+            </IsOnViewport>
+          ))}
+        </ScrollView>
+      </OnViewport>
     </View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,114 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, { ReactElement, useCallback, useEffect, useRef } from 'react';
+import { View, DeviceEventEmitter, ScrollView, Dimensions } from 'react-native';
+import _ from 'lodash';
+
+// 특정 컴포넌트
+interface OnViewportProps {
+  children: ReactElement<typeof ScrollView>;
+  delay?: number;
 }
+
+// 구독-발행
+export const OnViewport = (props: OnViewportProps) => {
+  const { delay = 1000, children } = props;
+
+  const trackWithDelay = _.throttle(() => {
+    DeviceEventEmitter.emit('track');
+  }, delay);
+
+  const _onScroll = useCallback((event) => {
+    const childOnScroll = props.children.props.onScroll;
+
+    childOnScroll && childOnScroll(event);
+
+    trackWithDelay();
+  }, []);
+
+  // console.log('dd', props.children instanceof ScrollView);
+  // if(React.Children.only(children).type === ScrollView){
+  //   console.log('!!!')
+  // }
+
+  return (
+    React.cloneElement(props.children, {
+      onScroll: _onScroll,
+    })
+  );
+};
+
+interface IsOnViewportProps {
+  id?: string | number;
+  children: ReactElement;
+  onViewport: () => void;
+  // detectType: ''
+  // condition?: (measure: { x: number, y: number, width: number, height: number, pageX: number, pageY: number, exposureCount: number }) => boolean;
+  // countRange?: {
+  //   min: 0,
+  //   max: 1
+  // };
+}
+
+export const IsOnViewport = (props: IsOnViewportProps) => {
+  const { id, onViewport } = props;
+  // const exposureCount = useRef(0);
+
+  const ref = useRef(null);
+
+  const handleMeasure = (x, y, width, height, pageX, pageY) => {
+    const windowHeight = Dimensions.get('window').height;
+    const windowWidth = Dimensions.get('window').width;
+    // 조금이라고 걸치면, viewport 진입이라고 취급하기
+
+    // Viewport의 좌측 상단은 항상 (0,0) 이다.
+    const viewport = {
+      top: 0,
+      right: windowWidth,
+      bottom: windowHeight,
+      left: 0,
+    };
+
+    const element = {
+      top: pageY,
+      right: pageX + width,
+      bottom: pageY + height,
+      left: pageX,
+    };
+
+    // 완전히 들어온 경우,
+    const isFullyContained = element.top >= viewport.top && element.right <= viewport.right && element.bottom <= viewport.bottom && element.left >= viewport.left
+
+    // 조금이라도 걸친 경우
+
+    const status = isFullyContained;
+
+    onViewport && onViewport(status);
+  };
+
+  const handleLayout = () => {
+    if (ref.current) {
+      ref.current.measure(handleMeasure);
+    }
+  };
+
+
+  const handleScroll = () => {
+    // @ts-ignore
+    ref.current.measure(handleMeasure);
+  };
+
+  useEffect(() => {
+    const eventListener = DeviceEventEmitter.addListener('track', handleScroll);
+
+    return () => {
+      eventListener.remove();
+    };
+  }, []);
+
+  // TODO: React Navigation 지원하자.
+
+  return (
+    <View ref={ref} onLayout={handleLayout}>
+      {props.children}
+    </View>
+  );
+};
