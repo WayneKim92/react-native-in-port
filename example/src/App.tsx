39f579fd567e61b1347c51cb9e5e3d75026885b4
waynekim <eunchan0817@gmail.com>
2023-04-18 20:00:35 +0900

    feat: add examples for ScrollView and FlatList

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { View, ScrollView, ColorValue } from 'react-native';
+import { View, ScrollView, ColorValue, FlatList } from 'react-native';
 import { IsOnViewport, InViewPortScrollEmitter } from 'react-native-on-viewport';
 
 const Box = ({ backgroundColor }: { backgroundColor: ColorValue }) => <View
@@ -8,15 +8,15 @@ const Box = ({ backgroundColor }: { backgroundColor: ColorValue }) => <View
 
 const colors = ['blue', 'red', 'green', 'yellow', 'black'];
 
-export default function App() {
-  // @ts-ignore
+// @ts-ignore TODO: 별도의 스크린으로 빼자.
+const AppForScrollViewExample = () => {
   return (
     <View>
-      <InViewPortScrollEmitter throttleTime={1000}>
+      <InViewPortScrollEmitter throttleTime={100}>
         <ScrollView style={{ flexGrow: 1, flexShrink: 1 }} scrollEventThrottle={1}>
           {colors.map((color, index) => (
             <IsOnViewport
-              viewportMargin={{top:0}}
+              viewportMargin={{ top: 0 }}
               detectType={'incompletely'}
               onViewport={(status) => {
                 if (status) console.log(`${color} IN`);
@@ -30,6 +30,30 @@ export default function App() {
         </ScrollView>
       </InViewPortScrollEmitter>
     </View>
+  );
+};
 
+const AppForFlatListExample = () => {
+  return (
+    <View>
+      <InViewPortScrollEmitter throttleTime={500}>
+        <FlatList data={colors} renderItem={(data) => {
+          return (
+            <IsOnViewport
+              viewportMargin={{ top: 0 }}
+              detectType={'incompletely'}
+              onViewport={(status) => {
+                if (status) console.log(`${data.item} IN`);
+                else console.log(`${data.item} OUT`);
+              }}
+            >
+              <Box backgroundColor={data.item} />
+            </IsOnViewport>
+          );
+        }} />
+      </InViewPortScrollEmitter>
+    </View>
   );
 }
+
+export default AppForFlatListExample
